813aa50c1d44d9cdba533a95ce8fb58c0a1866b8
Stefan Schmidt <stefan7schmidt@gmx.net>
2018-05-06 13:23:56 +0200

    task 2 and 4 resolved.

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,42 @@
 
 # Download and extract zip file
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile = "UCI_HAR_Dataset.zip", method = "curl")
-dateDownloaded <- date()
+file.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(file.url, destfile = "UCI_HAR_Dataset.zip", method = "curl")
+date.downloaded <- date()
 unzip(zipfile = "UCI_HAR_Dataset.zip")
 
 # Load the data that is spread across several files
 train.subject <- read.table("UCI HAR Dataset/train/subject_train.txt")  # subject id
 train.activity <- read.table("UCI HAR Dataset/train/y_train.txt")       # activity id
-train.data <- read.table("UCI HAR Dataset/train/X_train.txt")           # actual data
+train.data <- read.table("UCI HAR Dataset/train/X_train.txt")           # actual measurement data
 
 test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test.activity <- read.table("UCI HAR Dataset/test/y_test.txt")
 test.data <- read.table("UCI HAR Dataset/test/X_test.txt")
 
-features <- read.table("UCI HAR Dataset/features.txt")
-activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)                # feature labels
+activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)  # activity labels
 
-# Create the train and test data frames from the columns the
+# Bind the columns of train/test data sets
 train <- cbind(train.subject, train.activity, train.data)
 test <- cbind(test.subject, test.activity, test.data)
 
-# 1. Merge the training and the test sets to create one data set
+# 1. Merge the training and the test sets to create one data set.
 all.data <- rbind(train, test)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement
-
+# since
+feature.indexes <- grep('-(mean|std)', features[, 2])
+# increment feature.index, since we have two additional columns (subject id, activity id) in all.data
+std.mean.indexes <- c(1:2, feature.indexes + 2)
+std.mean.data <- all.data[, std.mean.indexes]
 
 # 3. Use descriptive activity names to name the activities in the data set
-
+#std.mean.data[, 2] <- as.factor(std.mean.data[, 2], levels = activity.labels[, 1], labels = activity.labels[, 2])
 
 # 4. Appropriately label the data set with descriptive variable names
+feature.names <- grep('-(mean|std)', features[, 2], value = TRUE)
+names(std.mean.data) <- c("subject", "activity", feature.names)
 
 
 # 5. From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
