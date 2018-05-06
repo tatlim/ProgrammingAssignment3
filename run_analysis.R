eb6d6cb82a31ddbf7baa65f002812f832657e919
Stefan Schmidt <stefan7schmidt@gmx.net>
2018-05-06 09:10:45 +0200

    data loaded and train/test data frames merged.

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,28 @@
 
 # Download and extract zip file
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile = "UCI_HAR_Dataset.zip")
+download.file(fileUrl, destfile = "UCI_HAR_Dataset.zip", method = "curl")
 dateDownloaded <- date()
 unzip(zipfile = "UCI_HAR_Dataset.zip")
 
-# 1. Merge the training and the test sets to create one data set
+# Load the data that is spread across several files
+train.subject <- read.table("UCI HAR Dataset/train/subject_train.txt")  # subject id
+train.activity <- read.table("UCI HAR Dataset/train/y_train.txt")       # activity id
+train.data <- read.table("UCI HAR Dataset/train/X_train.txt")           # actual data
 
+test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test.activity <- read.table("UCI HAR Dataset/test/y_test.txt")
+test.data <- read.table("UCI HAR Dataset/test/X_test.txt")
+
+features <- read.table("UCI HAR Dataset/features.txt")
+activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# Create the train and test data frames from the columns the
+train <- cbind(train.subject, train.activity, train.data)
+test <- cbind(test.subject, test.activity, test.data)
+
+# 1. Merge the training and the test sets to create one data set
+all.data <- rbind(train, test)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement
 
